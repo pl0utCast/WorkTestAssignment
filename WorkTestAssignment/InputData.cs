39daccbd8faa9309bf326fafa43df97cf6cbb6db
pl0utCast <eddybor@gmail.com>
2025-02-26 07:25:03 +0300

    1) Обработка поля "ФИО". Оставить возможность ввода только буквенных символов 2) Поля "Телефон" и "Паспорт" должны быть полностью заполнены 3) Убрать возможность сохранения если вообще ничего не введено 4) Добавить проверку на дублирование данных(в базе уже есть пользователь с таким же номером телефона или паспорта) 5) В маске поля "Телефон" возможность ввода кода не только +7

---
+++ WorkTestAssignment/InputData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WorkTestAssignment
+{
+    /// <summary>
+    /// Класс, представляющий данные пользователя, такие как ФИО, телефон и паспортные данные.
+    /// </summary>
+    public class InputData
+    {
+        [JsonPropertyName("FIO")]
+        public string FIO { get; set; }
+
+        [JsonPropertyName("Phone")]
+        public string Phone { get; set; }
+
+        [JsonPropertyName("Passport")]
+        public string Passport { get; set; }
+
+        public InputData(string fIO, string phone, string passport)
+        {
+            FIO = fIO;
+            Phone = phone;
+            Passport = passport;
+        }
+    }
+}
--- WorkTestAssignment/MainWindow.xaml.cs
+++ WorkTestAssignment/MainWindow.xaml.cs
@@ -23,8 +23,26 @@ namespace WorkTestAssignment
     /// </summary>
     public partial class MainWindow : Window
     {
-        // Стек для хранения данных, который будет сериализован и десериализован в/из файла
+        //Стек для хранения данных, который будет сериализован и десериализован в/из файла
         private Stack<InputData> stack;
+
+        //Предыдущее корректное значение для ФИО
+        private string previousText = "";
+
+        //Предыдущее корректное значение для кода страны
+        private string previousNum = "";
+
+        //Шаблон для недопустимых символов
+        private string specialChars = @"[!""#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]";
+
+        //Регулярное выражение проверки корректности ФИО
+        private static readonly Regex rgx = new Regex(@"^[\p{L}]+$");
+
+        //Проверка на соответствие регулярному выражению
+        private static bool IsTextAllowed(string text)
+        {
+            return rgx.IsMatch(text);
+        }
 
         public MainWindow()
         {
@@ -50,16 +68,85 @@ namespace WorkTestAssignment
         /// <param name="e"></param>
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            if (MTB_Passport.Text == "____ ______" && MTB_Phone.Text == "(___)___-__-__" && TB_FIO.Text == "")
+            {
+                System.Windows.MessageBox.Show("Для сохранения записи заполните поля выше");
+                return;
+            }
+
+            bool error = false;
+            string errorText = "";
+
+
+            if (TB_FIO.Text == "")
+            {
+                errorText = "Поле 'ФИО' не должно быть пустым";
+                error = true;
+            }
+
+            if (TB_PhoneCode.Text.Equals(""))
+            {
+                errorText += "\nТелефонный код страны не должен быть пустым";
+                error = true;
+            }
+
+            if (MTB_Phone.Text.Contains("_"))
+            {
+                errorText += "\nПоле 'Телефон' должно быть заполнено целиком";
+                error = true;
+            }
+
+            if (MTB_Passport.Text.Contains("_"))
+            {
+                errorText += "\nПоле 'Паспорт (Серия и номер)' должно быть заполнено целиком";
+                error = true;
+            }
+
+            if (error)
+            {
+                System.Windows.MessageBox.Show(errorText);
+                return;
+            }
+
+            int phoneCode = Int32.Parse(TB_PhoneCode.Text);
+
+            string phoneCodeStr = phoneCode.ToString();
+
+            string phoneNumber = "+" + phoneCodeStr + MTB_Phone.GetLineText(0);
 
             using (FileStream fs = new FileStream("inputData.json", FileMode.Open))
             {
-                InputData inputData = new InputData(TB_FIO.GetLineText(0), MTB_Phone.GetLineText(0), MTB_Passport.GetLineText(0));
+                InputData inputData = new InputData(TB_FIO.GetLineText(0), phoneNumber, MTB_Passport.GetLineText(0));
+                //Проверка на дублирование
+                error = false;
+                errorText = "";
+                foreach(InputData data in stack)
+                {
+                    if (data.Phone == phoneNumber)
+                    {
+                        error = true;
+                        errorText = "Запись с таким номером телефона уже существует";
+                    }
+                    if (data.Passport == inputData.Passport)
+                    {
+                        error = true;
+                        errorText += "\nЗапись с такими паспортными данными уже существует";
+                    }
+                }
+
+                if (error)
+                {
+                    System.Windows.MessageBox.Show(errorText);
+                    return;
+                }
+
                 stack.Push(inputData);
                 JsonSerializer.Serialize<Stack<InputData>>(fs, stack);
                 System.Windows.MessageBox.Show("Сохранение прошло успешно");
             }
 
         }
+
 
         /// <summary>
         /// Обработчик события для инициализации окна, при котором данные из файла десериализуются в стек
@@ -89,28 +176,156 @@ namespace WorkTestAssignment
             stack.Clear();
             System.Windows.MessageBox.Show("Файл был успешно очищен");
         }
-    }
 
-
-    /// <summary>
-    /// Класс, представляющий данные пользователя, такие как ФИО, телефон и паспортные данные.
-    /// </summary>
-    public class InputData
-    {
-        [JsonPropertyName("FIO")]
-        public string FIO { get; set; }
-
-        [JsonPropertyName("Phone")]
-        public string Phone { get; set; }
-
-        [JsonPropertyName("Passport")]
-        public string Passport { get; set; }
-
-        public InputData(string fIO, string phone, string passport)
+        /// <summary>
+        /// Обработчик события ввода текста в текстовое поле для имени и фамилии,
+        /// Запрещает ввод символов, если они не являются буквенными.
+        /// </summary>
+        private void TB_FIO_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            FIO = fIO;
-            Phone = phone;
-            Passport = passport;
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        /// <summary>
+        /// Обработчик события изменения текста в текстовом поле для имени и фамилии.
+        /// Проверяет, содержит ли текст цифры или специальные символы. 
+        /// Если содержатся, возвращает текст к предыдущему значению.
+        /// </summary>
+        private void TB_FIO_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+
+            //Проверяем, содержит ли текст цифры
+            if (Regex.IsMatch(textBox.Text, @"\d+") || Regex.IsMatch(textBox.Text, specialChars))
+            {
+                //Если содержится, возвращаем текст к предыдущему значению
+                textBox.Text = previousText;
+                //Устанавливаем курсор в конец текста
+                textBox.SelectionStart = textBox.Text.Length;
+            }
+            else
+            {
+                //Если нет, сохраняем текущее значение как предыдущее
+                previousText = textBox.Text;
+            }
+
+        }
+
+        /// <summary>
+        /// Обработчик события ввода текста в текстовое поле для кода телефона.
+        /// Запрещает ввод символов, если они не являются цифрами.
+        /// </summary>
+        private void TB_PhoneCode_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = IsTextAllowed(e.Text);
+        }
+
+        /// <summary>
+        /// Обработчик события изменения текста в текстовом поле для кода телефона.
+        /// Проверяет, содержит ли текст буквы, пробелы или специальные символы. 
+        /// Если содержатся, возвращает текст к предыдущему значению.
+        /// </summary>
+        private void TB_PhoneCode_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           var textBox = sender as TextBox;
+
+            // Проверяем, содержит ли текст буквы или пробелы или спецсимволы
+            if (Regex.IsMatch(textBox.Text, @"\p{L}") || Regex.IsMatch(textBox.Text, @"\s") || Regex.IsMatch(textBox.Text, specialChars))
+            {
+                // Если содержится, возвращаем текст к предыдущему значению
+                textBox.Text = previousNum;
+                // Устанавливаем курсор в конец текста
+                textBox.SelectionStart = textBox.Text.Length;
+            }
+            else
+            {
+                // Если нет, сохраняем текущее значение как предыдущее
+                previousNum = textBox.Text;
+            }
+        }
+
+        /// <summary>
+        /// Обработчик события нажатия клавиш в текстовом поле для кода телефона,
+        /// Запрещает ввод пробела.
+        /// </summary>
+        private void TB_PhoneCode_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// Обработчик события потери фокуса в текстовом поле для имени и фамилии,
+        /// Проверяет, пустое ли поле. Если пустое, отображает сообщение об ошибке.
+        /// </summary>
+        private void TB_FIO_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (TB_FIO.Text.Equals(""))
+            {
+                Label_ErrorFIO.Visibility = Visibility.Visible;
+                Label_ErrorFIO.Content = "Поле не должно быть пустым";
+            }
+            else
+            {
+                Label_ErrorFIO.Visibility = Visibility.Hidden;
+                Label_ErrorFIO.Content = string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Обработчик события потери фокуса в текстовом поле для кода телефона,
+        /// Проверяет, пустое ли поле. Если пустое, отображает сообщение об ошибке.
+        /// </summary>
+        private void TB_PhoneCode_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (TB_PhoneCode.Text.Equals(""))
+            {
+                Label_ErrorPhone.Visibility = Visibility.Visible;
+                Label_ErrorPhone.Content = "Код страны не должен быть пустым";
+            }
+            else
+            {
+                Label_ErrorPhone.Visibility = Visibility.Hidden;
+                Label_ErrorPhone.Content = string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Обработчик события потери фокуса в маске ввода телефона,
+        /// Проверяет, заполнено ли поле полностью. Если нет, отображает сообщение об ошибке.
+        /// </summary>
+        private void MTB_Phone_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (MTB_Phone.Text.Contains("_"))
+            {
+                Label_ErrorPhone.Visibility = Visibility.Visible;
+                Label_ErrorPhone.Content = "Поле должно быть заполнено целиком";
+            }
+            else
+            {
+                Label_ErrorPhone.Visibility = Visibility.Hidden;
+                Label_ErrorPhone.Content = string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Обработчик события потери фокуса в маске ввода паспорта,
+        /// Проверяет, заполнено ли поле полностью. Если нет, отображает сообщение об ошибке.
+        /// </summary>
+        private void MTB_Passport_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (MTB_Passport.Text.Contains("_"))
+            {
+                Label_ErrorPassport.Visibility = Visibility.Visible;
+                Label_ErrorPassport.Content = "Поле должно быть заполнено целиком";
+            }
+            else
+            {
+                Label_ErrorPassport.Visibility = Visibility.Hidden;
+                Label_ErrorPassport.Content = string.Empty;
+            }
         }
     }
 }
